77851c4de03c43e9a7c9099d73475f7c34ec20b5
tss-671 <tss-671@yandex.ru>
2021-05-30 22:09:57 +0300

    Добалена корзина, модульный тест

---
+++ CRM-BL/Model/Cart.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM_BL.Model
+{
+    public class Cart
+    {
+        public Customer Customer { get; set; }
+        public Dictionary<Product, int> Products { get; set; }
+        public decimal Price => GetAll().Sum(p => p.Price);
+
+        public Cart(Customer customer)
+        {
+            Customer = customer;
+            Products = new Dictionary<Product, int>();
+        }
+
+        public void Add(Product product)
+        {
+            if (Products.TryGetValue(product, out int count))
+            {
+                Products[product] = ++count;
+            }
+            else
+            {
+                Products.Add(product, 1);
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach (var product in Products.Keys)
+            {
+                for (int i = 0; i < Products[product]; i++)
+                {
+                    yield return product;
+                }
+            }
+        }
+
+        public List<Product> GetAll()
+        {
+            var result = new List<Product>();
+            foreach (Product i in this)
+            {
+                result.Add(i);
+            }
+            return result;
+        }
+    }
+}
